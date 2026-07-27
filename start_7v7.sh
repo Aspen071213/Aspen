0d20083b02da4f494f7e7b845c2eba0935def61a
Aspen <1492594564@qq.com>
2026-07-28 02:37:47 +0800

    修改启动球场

--- start_7v7.sh
+++ start_7v7.sh
@@ -5,5 +5,5 @@ host=${1:-localhost}
 port=${2:-60000}
 
 for i in {1..7}; do
-  python3 run_player.py --host $host --port $port -n $i -t MujocoCodebase -f hl_adult &
+  python3 run_player.py --host $host --port $port -n $i -t MujocoCodebase -f custom_36x55 &
 done
